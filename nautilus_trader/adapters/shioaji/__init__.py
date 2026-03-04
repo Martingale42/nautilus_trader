efdf34c13df00b57e2e201a1e4d9e94b71e4d9fe
Martingale42 <112751010@nccu.edu.tw>
2026-03-04 22:01:07 +0800

    feat(sinopac): add SinopacInstrumentProvider with load_all_async

--- nautilus_trader/adapters/shioaji/__init__.py
+++ nautilus_trader/adapters/shioaji/__init__.py
@@ -1,9 +1,12 @@
 from nautilus_trader.adapters.shioaji.constants import SINOPAC
 from nautilus_trader.adapters.shioaji.constants import SINOPAC_CLIENT_ID
 from nautilus_trader.adapters.shioaji.constants import SINOPAC_VENUE
+from nautilus_trader.adapters.shioaji.providers import ShioajiInstrumentProvider
+
 
 __all__ = [
     "SINOPAC",
     "SINOPAC_CLIENT_ID",
     "SINOPAC_VENUE",
+    "ShioajiInstrumentProvider",
 ]
