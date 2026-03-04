fc202aec1a5c838447671b26792f49671e74e24c
Martingale42 <112751010@nccu.edu.tw>
2026-03-04 21:16:15 +0800

    feat(sinopac): add Python adapter package with constants

---
+++ nautilus_trader/adapters/shioaji/__init__.py
@@ -0,0 +1,9 @@
+from nautilus_trader.adapters.shioaji.constants import SINOPAC
+from nautilus_trader.adapters.shioaji.constants import SINOPAC_CLIENT_ID
+from nautilus_trader.adapters.shioaji.constants import SINOPAC_VENUE
+
+__all__ = [
+    "SINOPAC",
+    "SINOPAC_CLIENT_ID",
+    "SINOPAC_VENUE",
+]
