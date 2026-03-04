1dff2298eb967dedb1b9c01d12d7baba30b5489d
Martingale42 <112751010@nccu.edu.tw>
2026-03-04 21:16:15 +0800

    feat(shioaji): add Python adapter package with constants

---
+++ nautilus_trader/adapters/shioaji/constants.py
@@ -0,0 +1,11 @@
+from typing import Final
+
+from nautilus_trader.model.identifiers import ClientId
+from nautilus_trader.model.identifiers import Venue
+
+SINOPAC: Final[str] = "SINOPAC"
+SINOPAC_VENUE: Final[Venue] = Venue(SINOPAC)
+SINOPAC_CLIENT_ID: Final[ClientId] = ClientId(SINOPAC)
+
+SHIOAJI_GATEWAY_URL: Final[str] = "http://localhost:8000"
+SHIOAJI_GATEWAY_WS_URL: Final[str] = "ws://localhost:8000/ws"
